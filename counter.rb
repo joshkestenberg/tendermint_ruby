d675a7391282bac35512c9de01a375fc8ecf5a04
joshkestenberg <joshkestenberg@gmail.com>
2017-04-17 13:54:31 -0400

    added set_option method

--- counter.rb
+++ counter.rb
@@ -3,8 +3,32 @@ require './types_services_pb'
 
 class Counter < Types::ABCIApplication::Service
 
+  @serial = false
+  @count = 0
+
   def echo(string, _call)
     Types::ResponseEcho.new(message: "#{string.message}")
+  end
+
+  def set_option(flag, _call)
+    if flag.key == "serial"
+      if flag.value == "on"
+        @serial = true
+        Types::ResponseSetOption.new(log: "serial is #{flag.value}")
+      elsif flag.value == "off"
+        @serial = false
+        Types::ResponseSetOption.new(log: "serial is #{flag.value}")
+      else
+        Types::ResponseSetOption.new(log: "value must be either on or off")
+      end
+    else
+      Types::ResponseSetOption.new(log: "request key must be 'serial'")
+    end
+  end
+
+  def deliver_tx(trans, _call)
+    # trans_data = trans.tx
+    Types::ResponseDeliverTx.new(data: "3")
   end
 
 end
--- counter_client.rb
+++ counter_client.rb
@@ -3,8 +3,4 @@ require './types_services_pb'
 
 def main
   stub = Types::ABCIApplication::Stub.new('localhost:46658', :this_channel_is_insecure)
-  message = stub.echo(Types::RequestEcho.new(message: "howdy")).message
-  p "#{message}"
 end
-
-main
